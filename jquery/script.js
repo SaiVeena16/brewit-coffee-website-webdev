476d0119b99f458a3d519043b3266a51cd41c20b
Veenadhari <165553984+SaiVeena16@users.noreply.github.com>
2024-07-02 16:50:30 +0530

    Add files via upload

---
+++ jquery/script.js
@@ -0,0 +1,10 @@
+$("#search-btn").on("click",function() {
+    $(".search-form").toggleClass("active");
+});
+$("#login-btn").on("click",function() {
+    $(".login-form").toggleClass("active");
+});
+$("#menu-bars").on("click",function() {
+    $(".drop-down").toggleClass("responsive");
+});
+
